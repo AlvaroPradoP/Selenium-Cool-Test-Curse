1152906a24bcc4d5e5222541d6a5d46b455f0307
AJPPC <AJPPC@DESKTOP-DF5CF9H>
2021-08-31 18:50:34 -0500

    Fisrt commit

---
+++ IDB-invest/src/main/java/New_Assigment_group/TestSelenium.java
@@ -0,0 +1,20 @@
+package New_Assigment_group;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+
+	public static void main(String[] args) {
+		String exePath = "./Driver/chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver driver = new ChromeDriver();
+		float f = 26594.5949f;
+		double d = 95296.654;
+		
+		
+		driver.get("http://www.google.com");
+
+	}
+
+}
